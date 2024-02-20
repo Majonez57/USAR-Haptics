a7b0b1960a890ce5d0c439509155ed84bfe77df5
Adrian Vecina <avecinat@gmail.com>
2024-02-20 17:23:47 +0000

    Initial April Tag Work

---
+++ spot/spot_image_processing.py
@@ -0,0 +1,22 @@
+import cv2
+from pupil_apriltags import Detector, Detection
+import cv2 as cv
+import numpy as np
+
+def rotate_image(image, source_name):
+        ### Make sure image is upwards facing
+        if source_name == 'frontleft_fisheye_image':
+            image = cv2.rotate(image, rotateCode=0)
+        elif source_name == 'right_fisheye_image':
+            image = cv2.rotate(image, rotateCode=1)
+        elif source_name == 'frontright_fisheye_image':
+            image = cv2.rotate(image, rotateCode=0)
+
+        return image
+
+# Returns all tag data
+def detectTagValue(image_grey, tag_family="tag36h11"):
+    detector = Detector()
+    detected = detector.detect(image_grey, estimate_tag_pose=False)[0]
+
+    return detected.tag_id
--- spot/spotodom.py
+++ spot/spotodom.py
@@ -1,29 +1,61 @@
-import sys
+from PIL import Image
+import numpy as np
+import cv2
+from spot_image_processing import rotate_image, detectTagValue
+from bosdyn.api import image_pb2
 import bosdyn.client
 import bosdyn.client.util
 from bosdyn.client.robot_state import RobotStateClient
+from bosdyn.client.image import ImageClient, build_image_request
 
 class Bridge:
     def __init__(self) -> None:
         print("Attempting Spot Connection")
-        self.robot_state_client = self.connectToSpot()
-
-    def connectToSpot(self):
-        
         address = input("Please enter Spot Robot IP: ")
-        
+
         # Creating robot Object
         try:
             sdk = bosdyn.client.create_standard_sdk('RobotStateClient')
             robot = sdk.create_robot(address)    
             bosdyn.client.util.authenticate(robot)
-            robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
-        
-            return robot_state_client
+            self.robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
+            self.image_client = robot.ensure_client(ImageClient.default_service_name)
+            # Get regular camera sources
+            self.image_sources = [src.name for src in self.image_client.list_image_sources() if 
+                                  src.image_type == image_pb2.ImageSource.IMAGE_TYPE_VISUAL and 'depth' not in src.name]
+
+
+
+
         except Exception as e:
             print("An error occured connecting to the robot: ")
             print(e)
-        
+
+    def detectedValues(self):
+        ## Find a april tag within an image source
+        ## Return the value of said tag
+
+        for i in self.image_sources:
+            # Get image
+            img_req = build_image_request(i, quality_percent=100,
+                                          image_format=image_pb2.Image.FORMAT_RAW)
+            response = self.image_client.get_image([img_req])
+
+            width = response[0].shot.image.cols
+            height = response[0].shot.image.rows
+
+            data = response[0].shot.image 
+
+            img_grey = np.array(Image.frombytes('P', (int(width), int(height)), data, i))
+
+            img_grey = rotate_image(img_grey, i)
+
+            value = detectTagValue(img_grey)
+
+            print(value)
+
+
+
     def getState(self):
         return self.robot_state_client.get_robot_state()
     
@@ -35,10 +67,14 @@ class Bridge:
         
         return transforms.child_to_parent_edge_map['odom'].parent_tform_child
     
+
+
 if __name__ == "__main__":
     b = Bridge()
     
     try:
         print(b.getOdom())
+        b.detectedValues()
     except Exception as e:
         print(":( adrian code sucks")
+        print(e)
