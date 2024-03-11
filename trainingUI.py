13931f96889a460e458d54c25e1da9b29ab7e586
Adrian Vecina <avecinat@gmail.com>
2024-03-11 12:53:24 +0000

    Added extra categories

--- trainingUI.py
+++ trainingUI.py
@@ -8,7 +8,7 @@ def create_button(root, pos, image_path, text, on_press):
     row, col = pos
     # Open and resize the image to fit the button
     image = Image.open(image_path)
-    image = image.resize((170, 170), Image.ANTIALIAS) 
+    image = image.resize((150, 150), Image.ANTIALIAS) 
     image = ImageTk.PhotoImage(image)
     
     # Create the button with image and text
@@ -53,20 +53,26 @@ def main():
     # Configure row and column weights to make buttons fill the space
     for i in range(1, 3):  # 2 rows
         root.grid_rowconfigure(i, weight=1)
-    for i in range(3):  # 3 columns
+    for i in range(4):  # 4 columns
         root.grid_columnconfigure(i, weight=1)
 
     # Shuffle Button Positions
-    pos = [(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+    pos = [(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2), (2,3)]
     shuffle(pos)
 
     # Create buttons with images and text
+    # Enviroment State
     create_button(root, pos[0], "resources/images/fire.jpg", "Fire", lambda: vest.display_pattern('Right', dur=1.5, intensity=80, angle=90))
     create_button(root, pos[1], "resources/images/biohaz.jpg", "Biohazard", lambda: vest.display_pattern('Chevrons', dur=1.5))
     create_button(root, pos[2], "resources/images/oxygen.jpg", "Low Oxygen", lambda: vest.display_pattern('Inward_heart_Spiral', dur=1.5))
+    # User Detections
     create_button(root, pos[3], "resources/images/alive.png", "Uninjured Person", lambda: vest.display_pattern('Heartx4',intensity=200, dur=2))
     create_button(root, pos[4], "resources/images/injured.png", "Injured Person", lambda: display_injured())
     create_button(root, pos[5], "resources/images/dead.jpg", "Dead Person", lambda: display_dead())
+    # Robotic State
+    create_button(root, pos[6], "resources/images/lost.png", "Robot Connection Lost", lambda: vest.display_pattern('Top_360',intensity=200, dur=2))
+    create_button(root, pos[7], "resources/images/robotIssue.jpg", "Robot Error", lambda: vest.display_pattern('Triple_Flip',intensity=200, dur=2))
+
 
     example_label = tk.Label(root, text="Press a Category to recieve message:", font=("Arial", 20, "bold"))
     example_label.grid(row=0, column=0, columnspan=3, pady=10)
